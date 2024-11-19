4778e741611d96d7dce8044798959131dd5d8de5
giordanoscerra <121350206+giordanoscerra@users.noreply.github.com>
2024-11-19 14:09:02 +0000

    formula's validation step

--- src/plugin/service.py
+++ src/plugin/service.py
@@ -1,6 +1,8 @@
 from . import repository
 from .repository import MapOps
 from typing import List
+from ..config.db import kpis_collection
+from sympy import sympify
 
 def getGranularityOperation(op):
     if op == 'sum':
@@ -31,4 +33,20 @@ def createKPI(
     children: List[str], 
     formula: str
 ):
+    expr = sympify(formula)
+    kpis_in_formula = {str(symbol) for symbol in expr.free_symbols}
+    
+    # Query MongoDB to check if these KPIs exist
+    # The query will look for any documents where the 'name' field is one of the KPIs
+    existing_kpis = kpis_collection.find({"name": {"$in": list(kpis_in_formula)}}, {"_id": 0, "name": 1})
+
+    # Extract the names of the KPIs that exist in the database
+    existing_kpi_names = {doc["name"] for doc in existing_kpis}
+    
+    # Return missing KPIs
+    missing_kpis = kpis_in_formula - existing_kpi_names
+    if missing_kpis:
+        print(f"The following KPIs are missing from the database: {missing_kpis}")
+        raise ValueError("Missing KPIs")
+
     repository.createKPI(name, children, formula)
--- test.py
+++ test.py
@@ -1,15 +1,7 @@
 from src.plugin import controller
 
-if __name__ == '__main__':
-    name = 'ast-yhccl1zjue2t'
-    kpi = 'working_ratio'
-    start_date = "2024-09-30 00:00:00"
-    end_date = "2024-10-07 00:00:00"
-    days = 7
-    ops = 'sum'
-    res = controller.filterKPI(name, kpi, start_date, end_date, days, ops)
-    print(res)
-    
+def working_ratio_definer():
+  
     children = []
     kpi = 'working_time'
     res = controller.getKPIByName(kpi)
@@ -22,3 +14,38 @@ if __name__ == '__main__':
     name = 'working_ratio'
     formula = 'working_time/offline_time'
     controller.createKPI(name, children, formula)
+    
+def energy_efficiency_definer():
+    
+    children = []
+    kpi = 'consumption'
+    res = controller.getKPIByName(kpi)
+    children.append(res['_id'])
+
+    kpi = 'cycles'
+    res = controller.getKPIByName(kpi)
+    children.append(res['_id'])
+
+    name = 'energy_efficiency'
+    formula = 'consumption/cycles'
+    controller.createKPI(name, children, formula)
+    
+def faulty_kpi_definer():
+    
+    children = []
+    name = 'faulty_kpi'
+    formula = 'bug/cycles'
+    controller.createKPI(name, children, formula)
+    
+def filterKPI_example(kpi):
+    name = 'ast-yhccl1zjue2t'
+    start_date = "2024-09-30 00:00:00"
+    end_date = "2024-10-07 00:00:00"
+    days = 7
+    ops = 'sum'
+    res = controller.filterKPI(name, kpi, start_date, end_date, days, ops)
+    print(res)
+
+if __name__ == '__main__':
+    faulty_kpi_definer()
+    #filterKPI_example('faulty_kpi')
